167e074e06ab2b9c8e7a301442c903656929af95
Your Name <you@example.com>
2024-05-16 19:22:12 +0000

    new try

--- sudoku.c
+++ sudoku.c
@@ -76,12 +76,24 @@ int columnasValidas(Node *n){
    }
    return 1;
 }
-/*
+
 int subMatricesValidas(Node *n){
    int array[10];
-   iniciarEnCero(array, 10);
+   int p,i,j;
+
+   for(int k = 0; k < 9; k++){
+      iniciarEnCero(array, 10);
+      for(p = 0; p < 9; p++){
+          i=3*(k/3) + (p/3);
+          j=3*(k%3) + (p%3);
+         
+          if(n->sudo[i][j] == 0) continue;
+          array[n->sudo[i][j]]++;
+          if(array[n->sudo[i][j]] > 1) return 0;
+      }
+   }
    return 1;
-}*/
+}
 
 int is_valid(Node* n){
     if(filasValidas(n) && columnasValidas(n))
